37ad5e8df016c5d71ff5b1f139edcb26bfdb0bbe
viktorija <viktorija11@gmail.com>
2020-07-13 18:54:46 +0200

    getStaticProps() with NextJs

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>…</Head>
+      <section className={utilStyles.headingMd}>…</section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <Link href="/blog">
+          <a>Users</a>
+        </Link>
+      </section>
+    </Layout>
+  )
+}
+
